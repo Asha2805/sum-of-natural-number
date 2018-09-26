51480ff7da929cf78f01203d782c658a0a15cde6
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-26 12:25:05 +0530

    Update and rename README.md to sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+#import java.util.*;
+public class sum
+{
+public static void main(String[] args)
+{
+int n;
+Scanner e=new Scanner(System.in);
+n=e.nextInt();
+if(n==0)
+System.out.println("0");
+else
+{
+int m=n*(n+1)/2;
+System.out.println(m);
+}
+}
+}
